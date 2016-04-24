17b176865783ea565c2b2dc1956c40756ece3ab7
Szymi97 <szymon_kaszuba97@wp.pl>
2016-04-24 23:15:55 +0200

    Działający serwomechanizm
    Serwo 6V. Dla 5V nie zachowuje się poprawnie.

--- main.c
+++ main.c
@@ -1,73 +1,97 @@
 #include "stm32f4xx_conf.h"
 #include "stm32f4xx_gpio.h"
 #include "stm32f4xx_rcc.h"
-#include "stm32f4xx.h"
-//-----//
-#include "defines.h"
-#include "tm_stm32f4_delay.h"
-#include "tm_stm32f4_hcsr04.h"
-#include "tm_stm32f4_gpio.h"
-#include <stdio.h>
-#define zielona GPIO_Pin_12
-#define pomaranczowa GPIO_Pin_13
-#define czerwona GPIO_Pin_14
-#define niebieska GPIO_Pin_15
-#define wszystkie GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15
-void czujnik(void)
-{
+#include "stm32f4xx_tim.h"
+#include "stm32f4xx_usart.h"
+#include "misc.h"
+#include "stm32f4xx_syscfg.h"
+#include "stm32f4xx_exti.h"
 
-}
-int main(void)
-{
-	SystemInit();
-
-	/* GPIOD Periph clock enable */
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-
-	GPIO_InitTypeDef  GPIO_InitStructure;
-	/* Configure PD12, PD13, PD14 and PD15 in output pushpull mode */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+void Conf_GPIO() {
+	/* piny sterujace kierunkiem silników */
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_7;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-	TM_HCSR04_t czujnik;
-
-	TM_DELAY_Init(); // funkcje opozniajace
-
-	  if (!TM_HCSR04_Init(&czujnik, GPIOD, GPIO_PIN_0, GPIOC, GPIO_PIN_1)) //jak nie udalo sie podlaczyc
-	  	  {																   //ECHO PD0, TRIGGER PC1
-	        while (1)
-	        {
-	            GPIO_SetBits(GPIOD, czerwona);
-	            Delayms(100);
-	        }
-	  	  }
+	/* piny PWM dla serwa i silnika*/
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_8;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+}
 
 
-	unsigned int i;
+void Conf_Timer4() {
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+	TIM_TimeBaseStructure.TIM_Period = 65500;
+	TIM_TimeBaseStructure.TIM_Prescaler = 83;
+	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
+	TIM_Cmd(TIM4, ENABLE);
+}
+
+void Conf_PWM() {
+	TIM_OCInitTypeDef TIM_OCInitStructure;
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OCInitStructure.TIM_Pulse = 0;
+	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+	/* KONFIGURACJA 1 KANALU */
+	TIM_OC1Init(TIM4, &TIM_OCInitStructure);
+	TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_TIM4);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	/* KONFIGURACJA 2 KANALU */
+	TIM_OC3Init(TIM4, &TIM_OCInitStructure);
+	TIM_OC3PreloadConfig(TIM4, TIM_OCPreload_Enable);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource8, GPIO_AF_TIM4);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	GPIO_SetBits(GPIOB, GPIO_Pin_6);
+	GPIO_SetBits(GPIOB, GPIO_Pin_8);
+}
+
+void serwo_w_lewo(){ TIM4->CCR3 = 1550; }
+void serwo_w_prawo(){ TIM4->CCR3 = 950; }
+void serwo_standard(){ TIM4->CCR3 = 1250; }
+
+int main(void)
+{
+	SystemInit();
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+
+	Conf_GPIO(); // inicjalizacja portow GPIO
+	Conf_Timer4(); // konfiguracja zegara TIM4
+	Conf_PWM(); // inicjalizacja i konfiguracja PWM
+
 
 
 	while(1)
 	{
-		        TM_HCSR04_Read(&czujnik);
-
-		        if (czujnik.Distance < 0) //blad
-		        {
-		        	GPIO_SetBits(GPIOD, czerwona);
-		        }
-		        else if (czujnik.Distance > 50) //ponad 50cm
-		        {
-		        	GPIO_SetBits(GPIOD, pomaranczowa);
-		        }
-		        else //miedzy 0 a 50 cm
-		        {
-		        	GPIO_SetBits(GPIOD, niebieska);
-		        }
-		        Delayms(100); //opoznienie
-		        GPIO_ResetBits(GPIOD, wszystkie);
+		serwo_standard();
+		for (int i = 0; i < 10000000; i++){}
+		serwo_w_lewo();
+		for (int i = 0; i < 10000000; i++){}
+		serwo_standard();
+		for (int i = 0; i < 10000000; i++){}
+		serwo_w_prawo();
+		for (int i = 0; i < 10000000; i++){}
 	}
 }
-
